23ae68bd9fd97cabb66d5c878ced618a6e3048c7
Yevhen <gl.evhen@gmail.com>
2024-10-17 12:56:34 +0200

    Run vat id validation on keydown event instead of keypress

--- Model/Form/Modifier/AddressFormModifiers.php
+++ Model/Form/Modifier/AddressFormModifiers.php
@@ -79,7 +79,10 @@ class AddressFormModifiers implements EntityFormModifierInterface
             return;
         }
 
-        $vatIdField->setAttribute('@keydown.debounce.300ms', '$dispatch(\'close-vat-message\'); $dispatch(\'vat-id-changed\', $event.target.value)');
+        $vatIdField->setAttribute(
+            '@keydown.debounce.300ms',
+            '$dispatch(\'close-vat-message\'); $dispatch(\'vat-id-changed\', $event.target.value)'
+        );
         $vatIdField->setAttribute('@change.debounce', '$dispatch(\'vat-id-changed\', $event.target.value)');
     }
 
